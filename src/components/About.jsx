aba7d361839c7f529e7ae0abe74464e58157c84a
lovvejeet <135307361+lovvejeet@users.noreply.github.com>
2024-06-06 16:40:27 +0530

    added moving eyes features

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,22 @@
 function About() {
   return (
-    <div className="w-full py-20 bg-[#CDEA68] rounded-tl-3xl rounded-tr-3xl text-[#000]">
-      <h1>
+    <div className="w-full p-20 bg-[#CDEA68] rounded-tl-3xl rounded-tr-3xl text-[#000]">
+      <h1 className="font-['Neue_Montreal'] text-[4vw] leading-[4.5vw] tracking-tight">
         Ochi is a strategic partner for fast-grow­ing tech businesses that need
         to raise funds, sell prod­ucts, ex­plain com­plex ideas, and hire great
         peo­ple.
       </h1>
+      <div className="w-full flex gap-5 border-t-[1px] border-[#a1b562] mt-20 pt-10">
+        <div className="w-1/2">
+          <h1 className="text-6xl">Our approach:</h1>
+          <button className="flex gap-10 items-center px-10 py-5 mt-10 rounded-full bg-zinc-900 text-white uppercase">
+            Read More
+            <div className="w-2 h-2 rounded-full bg-zinc-100"></div>
+          </button>
+        </div>
+        <div className="h-[70vh] w-1/2 bg-white rounded-3xl"></div>
+        <div></div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Eyes.jsx
@@ -0,0 +1,55 @@
+import { useEffect, useState } from "react";
+
+const Eyes = () => {
+  const [rotate, setRotate] = useState(0);
+  useEffect(() => {
+    window.addEventListener("mousemove", (e) => {
+      let mouseX = e.clientX;
+      let mouseY = e.clientY;
+
+      let deltaX = mouseX - innerWidth / 2;
+      let deltaY = mouseY - innerHeight / 2;
+
+      var angle = Math.atan2(deltaX, deltaY) * (180 / Math.PI);
+      setRotate(angle - 180);
+    });
+  }, []);
+  return (
+    <div className="eyes w-full h-screen overflow-hidden">
+      <div className="relative w-full h-full bg-cover bg-center bg-[url('https://ochi.design/wp-content/uploads/2022/05/Top-Viewbbcbv-1-1440x921.jpg')]">
+        <div className="absolute flex gap-10 h-10 top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%]">
+          <div className="flex justify-center items-center w-[15vw] h-[30vh] rounded-full bg-zinc-100 ">
+            <div className="flex justify-center items-center w-2/3 h-2/3 rounded-full bg-zinc-900 ">
+              <div className="relative w-[100%] h-2/3 rounded-full bg-zinc-900 ">
+                <div
+                  style={{
+                    transform: `translate(-50%,-50%) rotate(${rotate}deg)`,
+                  }}
+                  className="  absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] line w-full h-10 "
+                >
+                  <div className="w-7 h-7 rounded-full bg-zinc-100 "></div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="flex justify-center items-center w-[15vw] h-[30vh] rounded-full bg-zinc-100 ">
+            <div className="flex justify-center items-center w-2/3 h-2/3 rounded-full bg-zinc-900 ">
+              <div className="relative w-[100%] h-2/3 rounded-full bg-zinc-900 ">
+                <div
+                  style={{
+                    transform: `translate(-50%,-50%) rotate(${rotate}deg)`,
+                  }}
+                  className="absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] line w-full h-10 "
+                >
+                  <div className="w-7 h-7 rounded-full bg-zinc-100 "></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Eyes;
